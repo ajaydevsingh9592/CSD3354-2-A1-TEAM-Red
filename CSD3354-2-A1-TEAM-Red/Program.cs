8978d8b15881fc46c326efa238f288760b8420c5
visualstudio <visualstudio@230-PC20>
2019-02-21 16:46:51 -0500

    4:45

---
+++ CSD3354-2-A1-TEAM-Red/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD3354_2_A1_TEAM_Red
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
