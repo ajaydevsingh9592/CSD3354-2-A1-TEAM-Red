73add5127d1994268de1f69f219c4a5aa1ed0a81
visualstudio <visualstudio@230-PC20>
2019-02-21 16:50:54 -0500

    4:50

--- CSD3354-2-A1-TEAM-Red/Program.cs
+++ CSD3354-2-A1-TEAM-Red/Program.cs
@@ -6,10 +6,91 @@ using System.Threading.Tasks;
 
 namespace CSD3354_2_A1_TEAM_Red
 {
-    class Program
+    class UsingRecursion
     {
-        static void Main(string[] args)
+        // Create an application which 
+        // calculates the sum of all the numbers from n to m recursively:
+
+        public static int CalculateRecursively(int n, int m)
         {
+            int sum = n;
+
+            if (n < m)
+            {
+                n++;
+                return sum += CalculateRecursively(n, m);
+            }
+            return sum;
         }
+
+
     }
+
+    class CountrySide
+    {
+        static void Main()
+        {
+            new CountrySide().Run();
+        }
+
+
+        // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+        Village Maeland;
+        Village Helmholtz;
+        Village Alst;
+        Village Wessig;
+        Village Badden;
+        Village Uster;
+        Village Schvenig;
+
+        public void travelVillages(Village currentVillage)
+        {
+            if (currentVillage.isAstrildgeHere)
+            {
+                Console.WriteLine("I found Dear Astrildge in 0", currentVillage.VillageName);
+                Console.WriteLine("**** FEELING HAPPY ****");
+                return;
+            }
+            travelVillages(currentVillage.west);
+            travelVillages(currentVillage.east);
+        }
+
+
+        public void Run()
+        {
+            Alst = new Village("Alstr", false);
+            Schvenig = new Village("Schvenig", false);
+            Wessig = new Village("Wessig", false);
+            Alst.distanceToEastVillage = 14;
+            Alst.distanceToWestVillage = 19;
+            Alst.west = Schvenig;
+            Alst.east = Wessig;
+
+            //
+            Schvenig.west = null;
+            Schvenig.east = null;
+
+            //
+            Wessig.west = null;
+            Wessig.east = null;
+        }
+
+    }
+    class Village
+    {
+        public Village(string _villageName, bool _isAHere)
+        {
+            isAstrildgeHere = _isAHere;
+            VillageName = _villageName;
+        }
+
+        public Village west;
+        public Village east;
+        public string VillageName;
+        public int distanceToEastVillage;
+        public int distanceToWestVillage;
+        public bool isAstrildgeHere;
+    }
+
+
 }
